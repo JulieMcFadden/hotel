7b835c55ffdeed74eb63b4d7214fc8fb9d190dce
Julie McFadden <she@juliemcfadden.com>
2016-11-07 09:13:01 -0500

    starting point - empty MVC

---
+++ mvc/mvc/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Documentation";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Full Stack .NET Developer";
+
+            return View();
+        }
+    }
+}
